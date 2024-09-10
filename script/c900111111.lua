4534c66c5015a00e08886470a1c231b716d8d520
By-ZeroToInfinity <jgboadascamejo@gmail.com>
2024-09-10 00:58:31 -0400

    Delete script directory

--- script/c900111111.lua
+++
@@ -1,4 +0,0 @@
---Tierra de la Reincarnaxión
-function c900111111.initial_effect(c)
-	
-end
--- script/c900111112.lua
+++
@@ -1,6 +0,0 @@
---Reincarnaxión de Metal y Huesos
-
-local s,id = GetID()
-function s.initial_effect(c)
-	
-end
