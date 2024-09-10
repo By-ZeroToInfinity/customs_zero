15d7df71503b2c02e616f301f585c7e82893821a
By-ZeroToInfinity <jgboadascamejo@gmail.com>
2024-09-10 00:34:02 -0400

    Add files via upload

---
+++ script/c900111112.lua
@@ -0,0 +1,6 @@
+--Reincarnaxión de Metal y Huesos
+
+local s,id = GetID()
+function s.initial_effect(c)
+	
+end
---
+++ script/versiones de pruebas/bonapartano version 0.1/c900111110.lua
@@ -0,0 +1,36 @@
+--Reincarnax Bonapartano
+
+local s,id =GetID()
+function s.initial_effect(c)
+	--activar efecto
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetOperation(s.activate)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+end
+
+s.listed_names={900111111}
+
+function s.thfilter(c)
+	return c:IsCode(900111111) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstMatchingCard(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if tc then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+
